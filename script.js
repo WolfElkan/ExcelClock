5c64a8ee494fe941aff851ac4bed1d945d135715
Wolf Elkan <wolf.elkan@gmail.com>
2020-06-24 23:38:01 -0400

    My age

---
+++ script.js
@@ -0,0 +1,47 @@
+function type(obj) {
+	return obj.__proto__.constructor
+}
+
+var epoch1900 = 25569 * 86400 * 1000;
+
+$(document).ready(function() {
+	console.log('script.js')
+	setInterval(function() {
+		// var now = Number(new Date())+epoch1900
+		var now = Number(new Date()) - new Date(1993,2-1,7,5,27)
+		now /= 86400 * 1000;
+		now -= 4 / 24
+		now = String(now)
+		now = now.substr(0,now.indexOf('.')+6)
+		$('#excel').text(now)
+		$('#normal').text(new Date())
+	}, 864);
+	// }, 136);
+})
+
+function gcf(a, b) {
+	if (a > b) {
+		return gcf(a - b)
+	} else if (b > a) {
+		return gcf(b - a)
+	} else {
+		return a
+	}
+}
+
+function rebase(num, digits='0123456789XE') {
+	if (type(digits) == Number){
+		var base = digits
+		digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'.substr(0,base)
+	} else {
+		base = digits.length;
+	}
+	result = ''
+	while (num >= 1) {
+		num = Math.floor(num)
+		var digit = digits[num % base]
+		result = digit + result
+		num /= base
+	}
+	return result
+}
